d547d1994efa2c9eec8d55ccaf58cf73bb211f7a
Youn Seung Hwan <cxzyy62@gmail.com>
2021-09-18 16:55:49 +0900

    redis -> redis rab

--- src/pubsub.ts
+++ src/pubsub.ts
@@ -1,33 +1,33 @@
-import { PubSub } from "apollo-server-express";
+// import { PubSub } from "apollo-server-express";
 
 
-const pubsub = new PubSub();
-
-export default pubsub;
-
-
-// import { RedisPubSub } from 'graphql-redis-subscriptions';
-// //import * as Redis from 'ioredis';
-// const Redis = require('ioredis')
-// //var host =process.env.REDIS_URL;
-
-// const options = {
-//   host: "database-1.cwctaqcu532i.ap-northeast-2.rds.amazonaws.com",
-//   port: "5432",
-//   username: "postgresql",
-//   password:"postgresql",
-//   dbname:"postgres",
-//   retryStrategy: times => {
-//     // reconnect after
-//     return Math.min(times * 50, 2000);
-//   }
-// };
-
-// const pubsub = new RedisPubSub({
-//   publisher: new Redis(options),
-//   subscriber: new Redis(options)
-// });
+// const pubsub = new PubSub();
 
 // export default pubsub;
+
+
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+//import * as Redis from 'ioredis';
+const Redis = require('ioredis')
+//var host =process.env.REDIS_URL;
+
+const options = {
+  host: "redis-12337.c290.ap-northeast-1-2.ec2.cloud.redislabs.com",
+  port: "12337",
+  //username: "postgresql",
+  password:"Felc44rml7wcGCYPDexukSmAvSrpw6TF",
+  dbname:"redis-test",
+  retryStrategy: times => {
+    // reconnect after
+    return Math.min(times * 50, 2000);
+  }
+};
+
+const pubsub = new RedisPubSub({
+  publisher: new Redis(options),
+  subscriber: new Redis(options)
+});
+
+export default pubsub;
 
  
